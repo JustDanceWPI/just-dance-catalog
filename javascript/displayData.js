99cbf0bc3d9c0563568b84720d3e62167b3766eb
p-lau <plau@wpi.edu>
2019-12-06 12:55:40 -0500

    Fixed table script from posting duplicate tables

--- javascript/displayData.js
+++ javascript/displayData.js
@@ -2,15 +2,10 @@
 var url = "https://spreadsheets.google.com/feeds/list/1UVG4JetMIqrr9rcIVEtvmhw1FAAnGCMHCmwiv99TJGI/3/public/values?alt=json";
 
 $.getJSON(url, function(data) {
-
     var entry = data.feed.entry;
 
-    $(entry).each(function(){
-        // Column names are name, age, etc.
-        $("table").addClass("flex-container").append("<tr><th>Group</th><th>Song</th></tr>")
-        $.each(entry, function() {
-            $("table").addClass("flex-container").append("<tr><td>" + this.gsx$groupname.$t + "</td><td>" + this.gsx$songname.$t + "</td></tr>")});
-
-    });
+    $("table").addClass("flex-container").append("<tr><th>Group</th><th>Song</th></tr>")
+    $.each(entry, function() {
+    $("table").addClass("flex-container").append("<tr><td>" + this.gsx$groupname.$t + "</td><td>" + this.gsx$songname.$t + "</td></tr>")});
 
 });
