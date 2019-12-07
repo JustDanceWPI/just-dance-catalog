257e389cb2edb80ee30e847cbf3634bcf0ffe949
p-lau <plau@wpi.edu>
2019-12-07 18:30:03 -0500

    display tables

--- javascript/displayData.js
+++ javascript/displayData.js
@@ -1,11 +1,11 @@
 // Google Spreadsheet JSON data
 var url = "https://spreadsheets.google.com/feeds/list/1UVG4JetMIqrr9rcIVEtvmhw1FAAnGCMHCmwiv99TJGI/3/public/values?alt=json";
 
-let loadResponses = () => {$.getJSON(url, function(data) {
+$.getJSON(url, function(data) {
     let entry = data.feed.entry;
 
-    $("table").addClass("flex-container").append("<tr><th>Group</th><th>Song</th></tr>");
+    $("table").addClass("table table-dark").append("<tr><th>Time</th><th>Name</th><th>Song</th><th>Score</th></tr>");
     $.each(entry, function() {
-    $("table").addClass("flex-container").append("<tr><td>" + this.gsx$groupname.$t + "</td><td>" + this.gsx$songname.$t + "</td></tr>")});
+    $("table").addClass("table table-dark").append("<tr><td>" + this.gsx$timestamp.$t + "</td><td>" + this.gsx$groupname.$t + "</td><td>" + this.gsx$songname.$t + "</td><td>" + this.gsx$score.$t + "</td></tr>")});
 
-})};
+});
